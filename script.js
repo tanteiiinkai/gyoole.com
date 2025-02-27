ef9b727ddf64520c6593fa4d8d2b77bda94c20e8
blanket223 <xiaoq3427@gmail.com>
2025-02-27 17:45:27 +0900

    comitted

---
+++ script.js
@@ -0,0 +1,23 @@
+const kensakuran = document.querySelector(".kensakuran");
+const searchbutton = document.querySelector(".search");
+const basho = document.querySelector(".basho");
+const kensakukekka = document.querySelector(".kensakukekka");
+function show(thing) {
+    thing.classList.add("show");
+    thing.classList.remove("none");
+}
+function none(thing) {
+    thing.classList.add("none");
+    thing.classList.remove("show");
+}
+function search(event) {
+    event.preventDefault();
+    var ving = kensakuran.value;
+    if (ving === "フラット") {
+        show(kensakukekka);
+        none(basho);
+    } else if (ving === "マップ") {
+
+    }
+};
+searchbutton.addEventListenner('submit', search);
